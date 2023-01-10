cd2a8b32f1419758ddf7e2928a32c0039d03c557
profjaquetafine <88409934+profjaquetafine@users.noreply.github.com>
2023-01-10 20:40:15 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,17 @@
+
+	player1_name = localStorage.getItem("player1_name");
+	player2_name = localStorage.getItem("player2_name");
+
+function send() {
+	
+}
+
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+	
+}
+
